7b194b489c5fb4b5c8ed1cb66947d3b4367534c0
‘Hba25’ <bh25dev@outlook.com>
2026-05-27 12:20:24 +0100

    Created Player.java file for introduction of player turn swapping.

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Player {
+
+    private String name;
+
+    Player(String name){
+        this.name = name;
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
