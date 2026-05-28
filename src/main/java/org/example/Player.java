1a4ae2793ab8c2406689622451ddb3276934c517
‘Hba25’ <bh25dev@outlook.com>
2026-05-28 06:03:34 +0100

    Created point tracker for players, increment when snap achieved on their turn. Win statement for player with the most points, or a draw message. Introduced check to make sure the previous card is not null after snap reset. If it is, the snap check is skipped and the previouscard is set to the current card

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -3,11 +3,18 @@ package org.example;
 public class Player {
 
     private String name;
+    private int score;
 
     Player(String name){
         this.name = name;
+    }
 
+    public int getScore() {
+        return score;
+    }
 
+    public void setScore(int score) {
+        this.score = score;
     }
 
     public String getName() {
--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -13,11 +13,17 @@ public class Snap extends CardGame
         //Create Players
         Player player1 = new Player("Player 1");
         Player player2 = new Player("Player 2");
+        //Initialise players scores
+        int player1Score = player1.getScore();
+        int player2Score = player2.getScore();
+
+
 
 
 
         //Turn counter for players, 0 for P1, 1 for P2
-        int turnCount = 0;
+        //Make it an integer to call methods on it
+        Integer turnCount = 0;
         shuffleDeck();
         System.out.println("Game start!");
         //Create scanner
@@ -43,12 +49,8 @@ public class Snap extends CardGame
             sc.nextLine();
 
             Card currentCard = dealCard();
-            //Turn swap
-            if(turnCount == 0){
-                turnCount = 1;
-            } else{
-                turnCount =0;
-            }
+
+
 
             if (currentCard == null) {
                 System.out.println("No more cards");
@@ -57,34 +59,73 @@ public class Snap extends CardGame
             System.out.println(currentCard);
 
 
-            //Snap logic
-            if(currentCard.getSymbol().equals(previousCard.getSymbol())){
+            //Check if the previous card is null after a snap. If it is
+            if (!(previousCard == null)){
+                if(currentCard.getSymbol().equals(previousCard.getSymbol())){
 //                System.out.println("SNAP! You win!");
-                //Record exact time from snap
-                long beforeSnap = Instant.now().toEpochMilli();
-                System.out.println("Snap! Type snap within two seconds!!");
-                //Scanner to read user input
-                String snapOpp = sc.nextLine();
-                long afterSnap = Instant.now().toEpochMilli();
-                if (snapOpp.equals("snap") && afterSnap-beforeSnap<2000 ){
-                    System.out.println("SNAP! You win!");
-                    break;
+                    //Record exact time from snap
+                    long beforeSnap = Instant.now().toEpochMilli();
+                    System.out.println("Snap! Type snap within two seconds!!");
+                    //Scanner to read user input
+                    String snapOpp = sc.nextLine();
+                    long afterSnap = Instant.now().toEpochMilli();
+                    if (snapOpp.equals("snap") && afterSnap-beforeSnap<20000000 ){
+                        if(turnCount.equals(0)){
+                            System.out.println("Player 1 gets a point!");
+                            player1Score++;
+                        }
+                        else {
+                            System.out.println("Player 2 gets a point!");
+                            player2Score++;
+                        }
+                        previousCard = null;
+                    }
+                    else {
+                        System.out.println("Too slow!:( Point to other player! >:)");
+                        if(turnCount.equals(0)){
+                            System.out.println("Player 2 gets a point!");
+                            player2Score++;
+                        }
+                        else {
+                            System.out.println("Player 1 gets a point!");
+                            player1Score++;
+                        }
+                        previousCard = null;
+                    }
+
+
                 }
+                //If snap is not triggered
                 else {
-                    System.out.println("Time up! You lose :(");
-                    break;
-
+                    previousCard = currentCard;
                 }
-
 
             }
+            //Snap logic
+
+
+            //If the previous card is Null after a snap
             else{
                 previousCard = currentCard;
             }
-
+            //Turn swap
+            if(turnCount == 0){
+                turnCount = 1;
+            } else{
+                turnCount =0;
+            }
 
 
         }
+        System.out.println("Game over! Player 1: " + player1Score + " Player 2: " + player2Score);
+        if(player1Score>player2Score){
+            System.out.println("Player 1 - Wins!!!");
+        } else if (player2Score>player1Score) {
+            System.out.println("Player 2 - Wins!!!");
+        } else {
+            System.out.println("Draw :|");
+        }
+
 
 
 
